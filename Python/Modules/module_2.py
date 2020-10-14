592fe482eb5fd3d1aff56a8d6acc540927027502
Bhushan Pujar <bhushanbpujar@gmail.com>
2020-10-14 19:28:08 +0530

    14-10-2020

---
+++ Python/Modules/module_2.py
@@ -0,0 +1,7 @@
+import sys
+def square(x):
+    if (len(x)==2) and x[1].isdigit:
+        print(int(x[1])**2)
+    else:
+        print("Error: Enter only 1 number")
+square(sys.argv)
---
+++ Python/PythonPrograms/Addition of 2 numbers.py
@@ -0,0 +1,4 @@
+a=input("Enter the first Number:")
+b=input("\nEnter the second Number:")
+sum=float(a)+float(b)
+print("Sum: {0}".format(sum))
